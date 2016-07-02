ae4d3441f1f3bb99ad6b046e090ddd95b2509fb4
ram <bh.ram999@gmail.com>
2016-07-02 14:20:46 -0400

    modified seeds file to insert data into database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Auction.create(item_name: 'Book',seller: 'Kevin', end_date: '2016-12-12', status: 'Open', initial_bid: '40.50',created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Auction.create(item_name: 'Cycle',seller: 'Kevin', end_date: '2016-12-01', status: 'Open', initial_bid: '50.00',created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Auction.create(item_name: 'Pen',seller: 'Bob', end_date: '2016-08-20', status: 'Open', initial_bid: '10.00',created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Auction.create(item_name: 'Bag',seller: 'Mike', end_date: '2016-12-29', status: 'Open', initial_bid: '20.00',created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Auction.create(item_name: 'Bike',seller: 'Mike', end_date: '2017-01-12', status: 'Open', initial_bid: '5000.00',created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+
+Bid.create(auction_id: '1',bidder: 'Bob', bid: '40.96', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Bid.create(auction_id: '1',bidder: 'Mike', bid: '42.33', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Bid.create(auction_id: '1',bidder: 'A1', bid: '43.14', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Bid.create(auction_id: '2',bidder: 'Mike', bid: '51.00', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Bid.create(auction_id: '2',bidder: 'Bob', bid: '51.50', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Bid.create(auction_id: '4',bidder: 'A1', bid: '21.00', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
+Bid.create(auction_id: '4',bidder: 'Kevin', bid: '21.50', created_at: '2016-06-15 22:04:29',updated_at: '2016-06-15 22:04:29')
